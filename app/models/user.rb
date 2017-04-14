570d81c310274d6c0d7523a8e728ab23f6ecf51f
123tiagomartins <tiagomartins282@gmail.com>
2017-04-15 00:45:51 +0100

    Models

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+
+    validates :name, presence: true, 
+                     length: {minimum: 5, maximum: 30}
+    
+    validates :email, presence: true, 
+                      length: {maximum: 255},
+                      uniqueness: true
+
+    validates :password, presence: true,
+	   		length: {minimum: 5}
+
+    has_secure_password
+end
