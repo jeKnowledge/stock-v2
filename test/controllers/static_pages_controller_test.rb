eee045f284ebb30c38d4215d350217e5056f6356
123tiagomartins <tiagomartins282@gmail.com>
2017-04-22 00:53:52 +0100

    Home Page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,15 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
-  end
+	
+	def setup
+		@base_title = "STOCK"
+	end
+
+	test "should get home" do
+		get static_pages_home_url
+		assert_response :success
+		assert_select "title", "Home | #{@base_title}"
+	end
 
 end
