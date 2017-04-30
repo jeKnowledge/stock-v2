b622488b0d60038ed39348e49ebc47b89901867f
123tiagomartins <tiagomartins282@gmail.com>
2017-05-01 00:52:25 +0100

    Deleting users as Admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,29 @@
 # Users
 
-100.times do |u|
-	password = Faker::Internet.password
+User.create!(name: "super_user",
+             email: "admin@admin.com",
+             password: "admin123",
+             password_confirmation: "admin123",
+             admin: true)
 
-	User.create!(name: Faker::Name.name,
-							 email: Faker::Internet.email,
-							 password: password,
-							 password_confirmation: password)
+User.create!(name: "user_example",
+             email: "example@example.com",
+             password: "example123",
+             password_confirmation: "example123",
+             admin: true)
+
+100.times do |u|
+  password = Faker::Internet.password
+
+  User.create!(name: Faker::Name.name,
+               email: Faker::Internet.email,
+               password: password,
+               password_confirmation: password)
 end
 
 150.times do |i|
-	Item.create!(name: Faker::Lorem.characters(5),
-							 state: false)
+  Item.create!(name: Faker::Lorem.characters(5),
+               state: false)
 
 end
 
